c0cede83137382a0eb21aa314fe5a4946423b334
omryha <omry.hazut@mail.huji.ac.il>
2020-04-08 22:54:27 +0300

    Navbar component added

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { HttpClientModule } from "@angular/common/http";
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule } from "@angular/forms";
+import { AuthService } from "./_services/auth.service";
 
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
