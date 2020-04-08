4060d87204695eaa6efcd3d357213556ef0b3efa
omryha <omry.hazut@mail.huji.ac.il>
2020-04-08 23:24:53 +0300

    Form added

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: "app-value",
-  templateUrl: "./value.component.html",
-  styleUrls: ["./value.component.css"],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getValues();
   }
 
@@ -24,5 +25,13 @@ export class ValueComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log("cancelled");
+  }
+}
