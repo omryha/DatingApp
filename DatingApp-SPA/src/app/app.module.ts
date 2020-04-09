6dc6dd0f50a358ffe87cd3e48b82623c76639343
omryha <omry.hazut@mail.huji.ac.il>
2020-04-09 10:54:20 +0300

    Exceptions

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-import { NavComponent } from "./nav/nav.component";
-import { FormsModule } from "@angular/forms";
-import { AuthService } from "./_services/auth.service";
-import { HomeComponent } from "./home/home.component";
-import { RegisterComponent } from "./register/register.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptor } from './_services/error.interceptor';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [AuthService],
+  providers: [AuthService, ErrorInterceptor],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
