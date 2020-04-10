54f5b0ce47e8e4c05ccb5b6787600e0bcf8c0e3b
omryha <omry.hazut@mail.huji.ac.il>
2020-04-10 22:37:47 +0300

    .

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using(var scope = host.Services.CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
