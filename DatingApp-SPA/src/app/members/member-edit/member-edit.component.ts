8a7c8d96aa333b3080ac0806c5a7412ead6f57ef
omryha <omry.hazut@mail.huji.ac.il>
2020-04-13 23:07:25 +0300

    Update user API

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -10,10 +12,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm', { static: true }) editForm: NgForm;
-
   user: User;
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+
+  @ViewChild('editForm', { static: true }) editForm: NgForm;
+  // Alert a warning when closing the browser tab while editing
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -22,9 +37,14 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profile updated successfuly');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(
+      next => {
+        this.alertify.success('Profile updated successfuly');
+        this.editForm.reset(this.user);
+      }, error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
 }
