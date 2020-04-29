18e8ff327332d676aa77cd74e0b04e0503e3bad2
omryha <omry.hazut@mail.huji.ac.il>
2020-04-29 14:58:45 +0300

    Messages Service

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  messageContainer = 'Unread';
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private alertifyService: AlertifyService
+  ) {}
 
   ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.messages = data['messages'].result;
+      this.pagination = data['messages'].pagination;
+    });
   }
 
+  loadMessages() {
+    this.userService
+      .getMessages(
+        this.authService.decodedToken.nameid,
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage,
+        this.messageContainer
+      )
+      .subscribe(
+        (res: PaginatedResult<Message[]>) => {
+          this.messages = res.result;
+          this.pagination = res.pagination;
+        },
+        (error) => {
+          this.alertifyService.error(error);
+        }
+      );
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMessages();
+  }
 }
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -11,6 +11,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -39,6 +40,7 @@ export const appRoutes: Routes = [
       {
         path: 'messages',
         component: MessagesComponent,
+        resolve: { messages: MessagesResolver },
       },
       {
         path: 'lists',
